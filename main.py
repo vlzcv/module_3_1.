59b8d44c18f72212fb44559e9ccc207754aa291f
vlzcv <vlada.zaytseva.2009@mail.ru>
2024-07-09 15:44:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+calls = 0
+
+
+def count_calls(call):
+    global calls
+    calls += calls
+    return calls
+
+
+def string_info(string):
+    a=[]
+    a.append(len(string))
+    a.append(string.upper())
+    a.append(string.lower())
+    count_calls(1)
+    return a
+
+def is_contains(string_info, is_contains):
+    string_info.lower()
+    new_is_contains = []
+    count_calls(1)
+    for i in is_contains:
+        new_is_contains.append(i.lower())
+    return (string_info.lower() in new_is_contains)
+
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
